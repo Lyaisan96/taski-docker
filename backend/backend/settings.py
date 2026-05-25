1dfcb841c737a953c64f5d103b9d0780b8716a9c
Lyaisan Rezyapova <lyaisan.rezyapova@yandex.ru>
2026-05-25 17:04:26 +0500

    Fix ALLOWED_HOSTS for Taski

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123', '<your_domain>']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '81.26.178.64', 'mytaskiserver26.hopto.org']
 
 
 # Application definition
